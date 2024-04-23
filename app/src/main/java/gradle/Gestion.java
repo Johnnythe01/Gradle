9133d39ae24464c0a226411711f05a736bd89494
John <jbarragandela@nigul.cide.es>
2024-04-23 19:47:02 +0200

    subida del dia

--- app/src/main/java/gradle/Gestion.java
+++ app/src/main/java/gradle/Gestion.java
@@ -1,22 +1,50 @@
 package gradle;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Gestion {
+    private Map<Integer, List<String>> comandas;
+
+    public Gestion() {
+        this.comandas = new HashMap<>();
+    }
 
     public int crear_comanda() {
-        throw new UnsupportedOperationException("Unimplemented method 'crear_comanda'");
+        int nuevoIdComanda = generarNuevoIdComanda();
+        comandas.put(nuevoIdComanda, new ArrayList<>());
+        return nuevoIdComanda;
     }
 
-    public void afegir_plat_comanda(int idComanda, String string) {
-        throw new UnsupportedOperationException("Unimplemented method 'afegir_plat_comanda'");
+    public void afegir_plat_comanda(int idComanda) {
+        if (comandas.containsKey(idComanda)) {
+            List<String> platos = comandas.get(idComanda);
+            platos.add(nomPlat);
+            comandas.put(idComanda, platos);
+        }
     }
 
-	public void mostrar_comanda(int idComanda) {
-		throw new UnsupportedOperationException("Unimplemented method 'mostrar_comanda'");
-	}
+    public void mostrar_comanda(int idComanda) {
+        if (comandas.containsKey(idComanda)) {
+            System.out.println("Platos de la comanda " + idComanda + ":");
+            List<String> platos = comandas.get(idComanda);
+            for (String plat : platos) {
+                System.out.println(plat);
+            }
+        }
+    }
 
     public double calcular_total_comanda(int idComanda) {
-        throw new UnsupportedOperationException("Unimplemented method 'calcular_total_comanda'");
+        if (comandas.containsKey(idComanda)) {
+            List<String> platos = comandas.get(idComanda);
+            double total = 0;
+            for (String plat : platos) {
+                double precio = Menu.getPrecioPlato(plat);
+                total += precio;
+            }
+            return total;
+        }
     }
-
 }
